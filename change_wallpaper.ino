eb3b49e2d39d585ea29ae4f4fffc38fb9f02c028
Timophane <83704460+Timophane@users.noreply.github.com>
2021-05-05 16:45:43 +0200

    Update change_wallpaper.ino

--- change_wallpaper.ino
+++ change_wallpaper.ino
@@ -1,11 +1,13 @@
 #include <Keyboard.h>
 
-void typeKey(int key)
+void pressKey(int key) //press a key and release it after 50ms
 {
   Keyboard.press(key);
   delay(50);
   Keyboard.release(key);
 }
+
+String trad;
 
 void setup()
 {
@@ -19,62 +21,62 @@ void setup()
   Keyboard.press('r');
   Keyboard.releaseAll();
   delay(300);
-  String text=fr2en("powershell");
-  Keyboard.print(text);
+  trad=fr2en("powershell");
+  Keyboard.print(trad);
   delay(100);
-  typeKey(KEY_RETURN);
+  pressKey(KEY_RETURN);
   delay(500);
   
   //changing working directory
-  text=fr2en("cd C:");
-  Keyboard.print(text);
+  trad=fr2en("cd C:");
+  Keyboard.print(trad);
   printBSlash(1);
-  text=fr2en("Users");
-  Keyboard.print(text);
+  trad=fr2en("Users");
+  Keyboard.print(trad);
   printBSlash(1);
-  text=fr2en("Public");
-  Keyboard.print(text);
-  typeKey(KEY_RETURN);
+  trad=fr2en("Public");
+  Keyboard.print(trad);
+  pressKey(KEY_RETURN);
   delay(100);
 
-  //downloading the picture (you can change easily the link with the one of your choice
-  String text1=fr2en("Invoke-WebRequest  https://media.npr.org/assets/img/2013/05/06/ducky062way-23e257dfd081032928ffbd73768a7ddd8615f1f3.jpg -o ducky062way-23e257dfd081032928ffbd73768a7ddd8615f1f3.jpg");
-  Keyboard.print(text1);
-  typeKey(KEY_RETURN);
+  //downloading the picture (you can change easily the link with the one of your choice => "Invoke-WebRequest your_link -o the_name_of_the_picture")
+  trad=fr2en("Invoke-WebRequest  https://media.npr.org/assets/img/2013/05/06/ducky062way-23e257dfd081032928ffbd73768a7ddd8615f1f3.jpg -o ducky062way-23e257dfd081032928ffbd73768a7ddd8615f1f3.jpg");
+  Keyboard.print(trad);
+  pressKey(KEY_RETURN);
   delay(1000); //1 sec to download 
 
   //changing wallpaper in the in the registers
-  String text2=fr2en("reg add \"HKEY_CURRENT_USER");
-  Keyboard.print(text2);
+  trad=fr2en("reg add \"HKEY_CURRENT_USER");
+  Keyboard.print(trad);
   printBSlash(1);
-  text2=fr2en("Control Panel");
-  Keyboard.print(text2);
+  trad=fr2en("Control Panel");
+  Keyboard.print(trad);
   printBSlash(1);
-  text2=fr2en("Desktop\" /v Wallpaper /t REG_SZ /d C:");
-  Keyboard.print(text2);
+  trad=fr2en("Desktop\" /v Wallpaper /t REG_SZ /d C:");
+  Keyboard.print(trad);
   printBSlash(1);
- String a=fr2en("Users");
-  Keyboard.print(a);
+  trad=fr2en("Users");
+  Keyboard.print(trad);
   printBSlash(1);
-  a=fr2en("public");
-  Keyboard.print(a);
+  trad=fr2en("public");
+  Keyboard.print(trad);
   printBSlash(1);
-  a=fr2en("ducky062way-23e257dfd081032928ffbd73768a7ddd8615f1f3.jpg /f");
-  Keyboard.print(a);
-  typeKey(KEY_RETURN);
+  trad=fr2en("ducky062way-23e257dfd081032928ffbd73768a7ddd8615f1f3.jpg /f");
+  Keyboard.print(trad);
+  pressKey(KEY_RETURN);
   delay(200);
 
   //update the register (sometimes it don't work ¯\_(ツ)_/¯ )
-  String text3=fr2en("RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters , 1 , True");
-  Keyboard.print(text3);
-  typeKey(KEY_RETURN);
+  trad=fr2en("RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters , 1 , True");
+  Keyboard.print(trad);
+  pressKey(KEY_RETURN);
   delay(500);
   Keyboard.print("exit");
-  typeKey(KEY_RETURN);
+  pressKey(KEY_RETURN);
   Keyboard.end();
 }
 
-String fr2en(String text) //azerty to qwerty keyboard, found here: https://dyrk.org/2018/12/09/arduino-simuler-un-clavier-azerty/?replytocom=1879#respond (I work with an azerty keyboard)
+String fr2en(String text) //azerty to qwerty keyboard, found here: https://dyrk.org/2018/12/09/arduino-simuler-un-clavier-azerty/?replytocom=1879#respond
 {
   int i = 0;
   String _en = " =qwertyuiopasdfghjkl;zxcvbnQWERTYUIOPASDFGHJKL:ZXCVBNm,./M<>?1234567890!@#$%^&*()\"",
