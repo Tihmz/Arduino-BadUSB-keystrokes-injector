c36931eac00b9484714833ad3a0b7a6e3b17e8bf
Timophane <83704460+Timophane@users.noreply.github.com>
2021-06-09 14:47:21 +0200

    Update basic_script.ino

--- basic_script.ino
+++ basic_script.ino
@@ -1,5 +1,16 @@
-#include <Keyboard.h>
+// $ -> keyboardScanCode(48);
+// ' -> keyboardScanCode(33);
+// \ -> printCA(37);
+// [ -> printCA(1,34);
+// ] -> printCA(1,45);
+// { -> printCA(1,33);
+// } -> printCA(1,46);
+// | -> printCA(1,35);
+// > -> printMaj(1,100);
+// < -> keyboardScanCode(100);
 
+
+#include <Keyboard.h>
 
 void pressKey(int key)
 {
@@ -10,14 +21,25 @@ void pressKey(int key)
 
 void setup()
 {
-  // Begining the Keyboard stream
   Keyboard.begin();
 
-  //delay for debugging or safety
-  delay(5000); 
-  
-  // Ending stream */
+  //delay for debugging and safety
+  delay(5000);
+
+
   Keyboard.end();
+}
+
+String fr2en(String text) //azerty to qwerty keyboard, found here: https://dyrk.org/2018/12/09/arduino-simuler-un-clavier-azerty/?replytocom=1879#respond
+{
+  int i = 0;
+  String _en = " =qwertyuiopasdfghjkl;zxcvbnQWERTYUIOPASDFGHJKL:ZXCVBNm,./M<>?1234567890!@#$%^&*()\"-",
+         _fr = " =azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN,;:!?./ & \"'(- _  1234567890%)",
+         str = "";
+  while (text[i] != 0){
+    str = str + (String)_en[_fr.indexOf((String)text[i++])];
+  }
+  return str;
 }
 
 void Keyboardprint(String text)
@@ -30,20 +52,7 @@ void Keyboardprint(String text)
   }
 }
 
-String fr2en(String text)
-{
-  int i = 0;
-  String _en = " =qwertyuiopasdfghjkl;zxcvbnQWERTYUIOPASDFGHJKL:ZXCVBNm,./M<>?1234567890!@#$%^&*()\"-",
-         _fr = " =azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN,;:!?./ & \"'(- _  1234567890%)",
-         str = "";
-  while (text[i] != 0){
-    str = str + (String)_en[_fr.indexOf((String)text[i++])];
-  }
-  return str;
-}
-
-
-void keyboardScanCode(byte code)
+void keyboardScanCode(byte code) //to press special key like '\', found here: https://forum.arduino.cc/t/keyboard-h-et-antislash/565172/3 (thanks to nico78)
 {  
   Keyboard.press(code+136);  
   delay(4);  
@@ -51,7 +60,7 @@ void keyboardScanCode(byte code)
   delay(4); 
 }
 
-void printCA(int n, int s) //print with ctrl + alt
+void printCA(int n, int s)
 {    
   for (int i=0;i<n;i++)
   {
